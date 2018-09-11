ecb93f606419c09733451afa26e9439b9a3373bd
Patrick Riordan <t00202274@365s.ittralee.ie>
2018-09-11 13:07:41 +0100

    Initial commit

---
+++ src/set1.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class set1{
+    public static void main (String args[]){
+        int cost, noOfMuffins;
+        String name, YourClass, muffinsAsString;
+
+        name = JOptionPane.showInputDialog( "Please enter your name");
+        YourClass = JOptionPane.showInputDialog( "Please enter your class");
+        muffinsAsString = JOptionPane.showInputDialog( "Please enter how many snacks you want");
+
+        
+
+
+    }
+
+
+
+}
